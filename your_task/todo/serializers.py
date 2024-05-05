41d7fda8a0cac9ce635656b70f51bc3f09acf2b8
AsiaShalaldeh <arwwd2002@gmail.com>
2024-05-05 23:09:45 +0300

    Add tasks APIs (get, create, update, delete, search)

--- your_task/todo/serializers.py
+++ your_task/todo/serializers.py
@@ -5,3 +5,13 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ('task_id', 'title', 'description', 'completed')
+
+class TaskFormSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ('title', 'description')
+
+class TaskTableSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ('title', 'description', 'status')
